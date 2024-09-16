f3a6031874c6bfb85b0a99a18c3e6ba29e1d8372
anthonysub <anthoncoy151@gmail.com>
2024-09-15 19:37:58 -0600

    actualizacion de datos

--- practica2.js
+++ practica2.js
@@ -1,4 +1,4 @@
-// Definir la clase Persona
+// Definir la clase Vehiculo
 class Vehiculo {
   constructor(color, nombre, kilometraje, tamaño, cajacambios) {
     this.color = color;
@@ -9,36 +9,35 @@ class Vehiculo {
   }
 
   describirse() {
-    console.log(`El vehiculo es de  color  ${this.color} su nombre es  ${this.nombre}, kilometraje que tiene es de  ${this.kilometraje}, su tamaño es  ${this.tamaño} y la caja de cambios que tiene es ${this.cajacambios}`);
+    console.log(`El vehículo es de color ${this.color}, su nombre es ${this.nombre}, tiene un kilometraje de ${this.kilometraje}, su tamaño es ${this.tamaño} y la caja de cambios es ${this.cajacambios}.`);
   }
 }
 
-// Crear personas
+// Crear vehículos
 const Vehiculo1 = new Vehiculo("rojo", "Spark", 25000, "pequeño", "manual");
 const Vehiculo2 = new Vehiculo("azul", "Camión", 150000, "grande", "automático");
 const Vehiculo3 = new Vehiculo("verde", "Moto", 5000, "pequeño", "manual");
 
-
-// Función para guardar personas en localStorage
+// Función para guardar vehículos en localStorage
 function guardarVehiculosEnLocalStorage(vehiculolist) {
-  const vehiculosJSON = JSON.stringify(vehiculolist); // Convertir el array de personas a JSON
+  const vehiculosJSON = JSON.stringify(vehiculolist); // Convertir el array de vehículos a JSON
   localStorage.setItem('Vehiculo', vehiculosJSON); // Guardar en localStorage
 }
 
-// Guardar los objetos persona en un array
+// Guardar los objetos vehículo en un array
 const vehiculolist = [Vehiculo1, Vehiculo2, Vehiculo3];
 guardarVehiculosEnLocalStorage(vehiculolist);
 
-// Función para recuperar personas de localStorage
+// Función para recuperar vehículos de localStorage
 function obtenerVehiculosDeLocalStorage() {
   const vehiculosJSON = localStorage.getItem('Vehiculo'); // Obtener el string JSON de localStorage
   if (vehiculosJSON) {
     const VehiculosArray = JSON.parse(vehiculosJSON); // Convertir el string JSON a un array de objetos
-    return VehiculosArray.map(p => new Vehiculo(p.color, p.nombre, p.kilometraje, p.tamaño, p.cajacambios)); // Volver a crear instancias de Persona
+    return VehiculosArray.map(p => new Vehiculo(p.color, p.nombre, p.kilometraje, p.tamaño, p.cajacambios)); // Volver a crear instancias de Vehiculo
   }
   return [];
 }
 
-// Recuperar y describir las personas
-const VehiculosRecuperadas = obtenerVehiculosDeLocalStorage();
-VehiculosRecuperadas.forEach(vehiculo => vehiculo.describirse()); // Llamar a describirse en cada persona
+// Recuperar y describir los vehículos
+const VehiculosRecuperados = obtenerVehiculosDeLocalStorage();
+VehiculosRecuperados.forEach(vehiculo => vehiculo.describirse()); // Llamar a describirse en cada vehículo
